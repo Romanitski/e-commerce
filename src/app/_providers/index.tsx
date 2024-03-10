9ae06f895d7a441ad01d62cddf03f006e0f16d1f
RVA <vromanitski@gmail.com>
2024-03-11 01:17:57 +0300

    update

--- src/app/_providers/index.tsx
+++ src/app/_providers/index.tsx
@@ -1,10 +1,10 @@
 'use client'
 
 import React from 'react'
-
 import { AuthProvider } from '../_providers/Auth'
 import { CartProvider } from '../_providers/Cart'
 import { ThemeProvider } from './Theme'
+import { FilterProvider } from './Filter'
 
 export const Providers: React.FC<{
   children: React.ReactNode
@@ -12,7 +12,9 @@ export const Providers: React.FC<{
   return (
     <ThemeProvider>
       <AuthProvider>
-        <CartProvider>{children}</CartProvider>
+        <FilterProvider>
+          <CartProvider>{children}</CartProvider>
+        </FilterProvider>
       </AuthProvider>
     </ThemeProvider>
   )
